7678fe3713925081f6765c5bd9c974ab1c4c02f8
Tanmay G Shetty <tanmay.121cr7@gmail.com>
2025-07-22 20:17:52 +0530

    Added new login functionality through passport

--- app.js
+++ app.js
@@ -7,10 +7,13 @@ const ejsMate = require('ejs-mate');
 const ExpressError = require('./utils/ExpressError.js');
 const session = require('express-session');
 const flash = require('connect-flash');
+const passport = require('passport');
+const Localstrategy = require('passport-local');
+const User = require('./models/user.js');
 
-
-const listings = require('./routes/listing.js');
-const reviews = require('./routes/review.js');
+const listingRouter = require('./routes/listing.js');
+const reviewRouter = require('./routes/review.js');
+const userRouter = require('./routes/user.js');
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/travelersheaven";
 async function main() {
@@ -50,6 +53,13 @@ app.get('/', (req, res) => {
 app.use(session(sessionOptions));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new Localstrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
@@ -57,8 +67,10 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use("/listings",listings);
-app.use("/listings/:id/reviews", reviews)
+
+app.use("/listings",listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/", userRouter);
 
 app.all("*",(req, res, next) => {
     next(new ExpressError(404,"Page Not Found!"));
